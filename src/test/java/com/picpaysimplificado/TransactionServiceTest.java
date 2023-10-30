48302e369f698e0c68b518b75445984a60e53d33
JoaoVNicacio <joaonicks99@gmail.com>
2023-10-29 23:12:49 -0300

    criando outros 3 casos

--- src/test/java/com/picpaysimplificado/TransactionServiceTest.java
+++ src/test/java/com/picpaysimplificado/TransactionServiceTest.java
@@ -90,4 +90,60 @@ class TransactionServiceTest {
 
         Assertions.assertEquals("Transação não autorizada", thrown.getMessage());
     }
+
+    @Test
+    @DisplayName("Should throw Exception when Transaction amount exceeds Sender's balance")
+    void createTransactionCase3() throws Exception {
+        User sender = new User(1L, "Maria", "Souza", "99999999901", "maria@gmail.com", "12345", new BigDecimal(10), UserType.COMMON);
+        User receiver = new User(2L, "Joao", "Souza", "99999999902", "joao@gmail.com", "12345", new BigDecimal(10), UserType.COMMON);
+
+        when(userService.findUserById(1L)).thenReturn(sender);
+        when(userService.findUserById(2L)).thenReturn(receiver);
+        TransactionDTO request = new TransactionDTO(new BigDecimal(15), 1L, 2L);
+
+        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+            transactionService.createTransaction(request);
+        });
+
+        Assertions.assertEquals("Saldo insuficiente para realizar a transação", thrown.getMessage());
+    }
+
+
+    @Test
+    @DisplayName("Should create transaction successfully when amount is zero")
+    void createTransactionCase4() throws Exception {
+        User sender = new User(1L, "Maria", "Souza", "99999999901", "maria@gmail.com", "12345", new BigDecimal(10), UserType.COMMON);
+        User receiver = new User(2L, "Joao", "Souza", "99999999902", "joao@gmail.com", "12345", new BigDecimal(10), UserType.COMMON);
+
+        when(userService.findUserById(1L)).thenReturn(sender);
+        when(userService.findUserById(2L)).thenReturn(receiver);
+        when(authService.authorizeTransaction(any(), any())).thenReturn(true);
+
+
+        TransactionDTO request = new TransactionDTO(BigDecimal.ZERO, 1L, 2L);
+        transactionService.createTransaction(request);
+
+
+        verify(repository, never()).save(any());
+        verify(userService, never()).saveUser(any());
+        verify(notificationService, never()).sendNotification(any(), any());
+    }
+
+
+    @Test
+    @DisplayName("Should throw Exception when Sender and Receiver are the same")
+    void createTransactionCase5() throws Exception {
+        User sender = new User(1L, "Maria", "Souza", "99999999901", "maria@gmail.com", "12345", new BigDecimal(10), UserType.COMMON);
+
+        when(userService.findUserById(1L)).thenReturn(sender);
+
+        TransactionDTO request = new TransactionDTO(new BigDecimal(5), 1L, 1L);
+
+        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+            transactionService.createTransaction(request);
+        });
+
+        Assertions.assertEquals("Não é possível realizar transação consigo mesmo", thrown.getMessage());
+    }
+
 }
